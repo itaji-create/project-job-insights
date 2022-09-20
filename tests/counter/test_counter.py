9a9f9a66a67e09b9edfec61144f5d908be2190fc
itaji-create <itaji.1999@gmail.com>
2022-09-20 12:52:16 -0300

    fix: muda test_counter de diretorio

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result = count_ocurrences("src/jobs.csv", "job")
+
+    assert type(result) == int
+    assert result == 3454
--- tests/test_counter.py
+++
@@ -1,8 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    result = count_ocurrences("src/jobs.csv", "job")
-
-    assert type(result) == int
-    assert result == 3454
