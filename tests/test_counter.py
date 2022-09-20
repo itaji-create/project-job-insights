3739a2b64bb3fb02ff7e76f729036070f585bbfe
itaji-create <itaji.1999@gmail.com>
2022-09-20 12:46:29 -0300

    fix: muda nome do teste

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,7 +1,8 @@
 from src.counter import count_ocurrences
 
 
-def test_count_ocurrences():
+def test_counter():
     result = count_ocurrences("src/jobs.csv", "job")
 
     assert type(result) == int
+    assert result == 3454
